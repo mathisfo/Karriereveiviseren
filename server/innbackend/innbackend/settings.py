30cbc379730aab9c9c758613dd42ff619fafb5d3
joningehe <jon.heggstad@gmail.com>
2021-02-22 18:32:19 +0100

    - Setup docker for all moving parts - Add docker-compose file - Runs 3 different containers - Container for backend - Container for frontend - Container for server connecting the two - Moved files from a parent folder to where they better belong

--- server/innbackend/innbackend/settings.py
+++ server/innbackend/innbackend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y2pyp!#esrdi=@%^vka&c)gm2*p0ky=&6h8)lf4wdob4*rw2l1'
+SECRET_KEY = "y2pyp!#esrdi=@%^vka&c)gm2*p0ky=&6h8)lf4wdob4*rw2l1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'innapp'
 ]
 
 SITE_ID = 1
