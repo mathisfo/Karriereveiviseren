6638e1b17bbe0b1b3beeb3d5216a03367dfba29d
Johanne <jtronstad@gmail.com>
2021-02-23 14:52:05 +0100

    implemented bootstrap #10

--- web/innfrontend/src/App.tsx
+++ web/innfrontend/src/App.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
-import "./App.css";
 import Course from "./components/Course";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 function App() {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <p>Yeet</p>
+      </Alert>
+    );
+  }
   return (
-    <div className="App">
-      <Course />
-    </div>
+    <Container className="p-3">
+      <Jumbotron>
+        <h1 className="header">INN01</h1>
+      </Jumbotron>
+      <Button variant="outline-primary" onClick={() => setShow(true)}>
+        Knapp
+      </Button>
+    </Container>
   );
 }
 
