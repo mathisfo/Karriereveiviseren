4f3d6f9b8b3f774706cf9a5e7cf6423ef3986a84
joningehe <jon.heggstad@gmail.com>
2021-02-25 16:38:03 +0100

    - Updated the model to now better fit what we will use in the final   product - Small but important change

--- server/innbackend/innapp/models.py
+++ server/innbackend/innapp/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 class Course(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    date = models.DateField()
+    title = models.CharField('tiltak', max_length=120)
+    startDate = models.DateTimeField(
+        'start dato', default=now)
+    endDate = models.DateTimeField('slutt dato', default=now)
+    description = models.TextField('beskrivelse')
+    # when restriction is 0, there is no restriction
+    restriction = models.IntegerField('spor', default=0)
+    # if we move to postgres, we can use arrayfield here
+    #other = models.ArrayField()
+
     def __str__(self):
         return self.title
+
+    def courseLifetime(self):
+        return self.endDate - self.startDate
+
+    def hasRestriction(self):
+        return False if self.restriction == 0 else True
