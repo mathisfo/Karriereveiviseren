b3e0fe8c7731069849a80bd6024f828095d3883a
mathiabs <mathiabs@stud.ntnu.no>
2021-02-25 14:44:20 +0100

    Add Card to for course information
    -create new component CourseCard
    -add some hardcoded examples (will be removed later)

--- web/innfrontend/src/App.tsx
+++ web/innfrontend/src/App.tsx
@@ -4,8 +4,12 @@ import logo from "./logo.svg";
 import Course from "./components/Course";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
+import Card from "react-bootstrap/Card";
+import CourseCard from "./components/CourseCard";
 
 function App() {
   const [show, setShow] = useState(true);
@@ -17,11 +21,59 @@ function App() {
       </Alert>
     );
   }
+
   return (
     <Container className="p-3">
+      <h1 className="header">Kursoversikt</h1>
+
       <Jumbotron>
-        <h1 className="header">INN01</h1>
+        <h1>Obligatorisk</h1>
+        <Container fluid>
+          <Row>
+            <Col>
+              <CourseCard
+                title="Forkurs"
+                text="Forkurs er noe alle må gjøre"
+                modul="felles"
+              />
+            </Col>
+            <Col>
+              <CourseCard
+                title="Norskkurs"
+                text="Norskkurs lærer deg det mest basice når det kommer til norsk språk og språkutvikling, helt fra Snorre til moderne helter som TIX. Alle ungdomer må dette"
+                modul="ungdom"
+              />
+            </Col>
+            <Col>
+              <CourseCard
+                title="Karrierekompetanse"
+                text="Utvikler din kompetanse for karriere. For elever i Spor 3"
+                modul="spor3"
+              />
+            </Col>
+            <Col>
+              <CourseCard
+                title="Karrierekompetanse"
+                text="Utvikler din kompetanse for karriere. For elever i Spor 2"
+                modul="spor2"
+              />
+            </Col>
+          </Row>
+        </Container>
+        <h1>Valgfrie kurs</h1>
+        <Container>
+          <Row>
+            <Col>
+              <CourseCard
+                title="Kulhetskurs"
+                text="Kurs for å bli kulere på byen. For ungdommer"
+                modul="ungdom"
+              />
+            </Col>
+          </Row>
+        </Container>
       </Jumbotron>
+
       <Button variant="outline-primary" onClick={() => setShow(true)}>
         Knapp
       </Button>
